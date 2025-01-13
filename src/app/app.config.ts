5add40e0279fb29e8febdb57abe2a12fb2af050b
Li Linyi <rocky@Lis-Laptop.local>
2025-01-13 23:21:18 +0700

    Install AngularFire2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-4d608","appId":"1:76174016163:web:756dceed32b7e17cdb4916","storageBucket":"friendlychat-4d608.firebasestorage.app","apiKey":"AIzaSyAd_09VvC7dSD8oW7NmlCAYMB2I4HuixJY","authDomain":"friendlychat-4d608.firebaseapp.com","messagingSenderId":"76174016163"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging())
   ],
 };
