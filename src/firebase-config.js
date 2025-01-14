d6501be21edfe1e5d24dd5c81d34085714cf56bd
Li Linyi <rocky@Lis-Laptop.local>
2025-01-14 21:44:19 +0700

    Adding Google Authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAd_09VvC7dSD8oW7NmlCAYMB2I4HuixJY",
+    authDomain: "friendlychat-4d608.firebaseapp.com",
+    projectId: "friendlychat-4d608",
+    storageBucket: "friendlychat-4d608.firebasestorage.app",
+    messagingSenderId: "76174016163",
+    appId: "1:76174016163:web:756dceed32b7e17cdb4916"
+  };
